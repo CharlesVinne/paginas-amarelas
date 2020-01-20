f076dfe2927c213fca96160578311ea0db5c954b
Vinicius <vinnesousa22@gmail.com>
2020-01-20 16:37:05 -0300

    https://github.com/CharlesVinne/paginas-amarelas.git

--- lista_telefonica/admin.py
+++ lista_telefonica/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
-# Register your models here.
+from lista_telefonica.models import Contato
+from lista_telefonica.models import Anuncio
+admin.site.register(Anuncio)
+admin.site.register(Contato)
--- lista_telefonica/models.py
+++ lista_telefonica/models.py
@@ -5,4 +5,14 @@ from django.db import models
 class Contato(models.Model):
     nome = models.CharField(max_length=255)
     telefone = models.CharField(max_length=20)
-    email = models 
+    email = models.EmailField()
+    def __str__(self):
+        return self.nome
+
+class Anuncio(models.Model):
+    nome_da_empresa = models.CharField(max_length=255)
+    descricao = models.TextField(max_length=255)
+    telefone_de_contato = models.CharField(max_length=14)
+    valor = models.DecimalField(max_digits=9, decimal_places=2, null=True)
+    def __str__(self):
+        return self.nome
